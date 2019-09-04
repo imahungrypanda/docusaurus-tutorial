33649043f6eaedfda23411fbb5078003c9c66553
Steve Olsen <steve@goco.io>
2019-09-04 11:14:51 -0500

    Add missing slash

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://imahungrypanda.github.io', // Your website URL
-  baseUrl: '/docusaurus-tutorial', // Base URL for your project */
+  baseUrl: '/docusaurus-tutorial/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
